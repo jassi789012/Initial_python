81045c6eb6f77e2ea181e4df37ddb07848a78eb7
jassi789012 <jassi789012singh@gmail.com>
2025-07-19 09:06:56 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,16 @@
+def greater(a,b):
+    if(a>b):
+        print("a is graeter..")
+    else:
+        print("b is greater..")
+
+
+
+
+a = int(input("Enter number a:"))
+b = int(input("Enter number b:"))
+greater(a,b)
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,25 @@
+# colors = ["red","green","blue","yellow"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+# print("Numbers:\n")
+# # for k in range(1,20001):
+#     print(k)
+
+# # range(x,y,z) 
+# # z = diffrence
+
+# # for k in range(1,21,2):
+# #     print(k)
+
+# i = 1
+# while(i<=30):
+#     print(i)
+#     i=i+1
+
+
+for i in range(0,4):
+    for j in range(0,4):
+        print("*")
