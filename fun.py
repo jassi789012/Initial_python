223a9a7e660f2ed8c243e07d1de3fce5dd47005b
jassi789012 <jassi789012singh@gmail.com>
2025-07-19 09:10:31 +0530

    Add files via upload

--- fun.py
+++ fun.py
@@ -1,16 +1,29 @@
-def greater(a,b):
+def table(n):
+    for i in range(1,11):
+        print(n," X ",i," = ",n*i)
+
+
+def gmean(a, b):
+    print((a*b)/(a+b))
+
+def chek(a, b):
     if(a>b):
-        print("a is graeter..")
+        print("a is greater")
+    elif(a<b):
+        print("b is greater")
     else:
-        print("b is greater..")
+        print("both are equal")
 
+def emp():
+    pass
 
+n = int(input("Enter any number: "))
+table(n)
 
+a = int(input("Enter a for gmean: "))
+b = int(input("Enter b for gmean: "))
+gmean(a, b)
 
-a = int(input("Enter number a:"))
-b = int(input("Enter number b:"))
-greater(a,b)
-
-
-
-
+a = int(input("Enter a to chek: "))
+b = int(input("Enter b to chek: "))
+chek(a, b)
